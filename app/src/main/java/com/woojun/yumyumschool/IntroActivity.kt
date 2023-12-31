03dafcf0fc8cd1f6f5456e519dbfb91c00e5e118
JangWoojun <woojun0107@naver.com>
2024-01-01 01:48:52 +0900

    feat: viewBinding 설정

--- app/src/main/java/com/woojun/yumyumschool/IntroActivity.kt
+++ app/src/main/java/com/woojun/yumyumschool/IntroActivity.kt
@@ -2,10 +2,17 @@ package com.woojun.yumyumschool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.woojun.yumyumschool.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+
+        }
     }
 }
