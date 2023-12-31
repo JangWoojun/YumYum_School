02f2c09f5c9ffd38e93d307f78d75972970ac320
JangWoojun <woojun0107@naver.com>
2024-01-01 06:52:19 +0900

    feat: Navigation 이동 설정

--- app/src/main/java/com/woojun/yumyumschool/MainActivity.kt
+++ app/src/main/java/com/woojun/yumyumschool/MainActivity.kt
@@ -2,10 +2,47 @@ package com.woojun.yumyumschool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import com.woojun.yumyumschool.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            val navController = findNavController(R.id.nav_host_fragment)
+
+            bottomNavigation.setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.home -> {
+                        navController.navigate(R.id.home)
+                        true
+                    }
+                    R.id.foodList -> {
+                        navController.navigate(R.id.foodList)
+                        true
+                    }
+                    R.id.community -> {
+                        navController.navigate(R.id.community)
+                        true
+                    }
+                    R.id.bestFood -> {
+                        navController.navigate(R.id.bestFood)
+                        true
+                    }
+                    else -> {
+                        false
+                    }
+                }
+            }
+
+            fab.setOnClickListener {
+
+            }
+
+        }
     }
 }
