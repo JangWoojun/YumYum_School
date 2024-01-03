d191eabded4feea99f46b2ff56cc6c98a9ef02a3
JangWoojun <woojun0107@naver.com>
2024-01-03 19:20:28 +0900

    feat: 날짜 버튼 기능 추가
    날짜 버튼 날짜 설정 및 클릭 효과 설정

--- app/src/main/java/com/woojun/yumyumschool/HomeFragment.kt
+++ app/src/main/java/com/woojun/yumyumschool/HomeFragment.kt
@@ -1,16 +1,24 @@
 package com.woojun.yumyumschool
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.google.android.material.card.MaterialCardView
 import com.woojun.yumyumschool.databinding.FragmentHomeBinding
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private val weekdays = getWeekdays()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +35,36 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
+            val weekText = arrayOf(mondayText, tuesdayText, wednesdayText, thursdayText, fridayText)
+            weekText.forEachIndexed { index, textView ->
+                textView.text = "${weekdays[index]}"
+            }
+
+            mondayButton.setOnClickListener {
+                resetButtonColors()
+                highlightButton(it)
+            }
+
+            tuesdayButton.setOnClickListener {
+                resetButtonColors()
+                highlightButton(it)
+            }
+
+            wednesdayButton.setOnClickListener {
+                resetButtonColors()
+                highlightButton(it)
+            }
+
+            thursdayButton.setOnClickListener {
+                resetButtonColors()
+                highlightButton(it)
+            }
+
+            fridayButton.setOnClickListener {
+                resetButtonColors()
+                highlightButton(it)
+            }
+
 
         }
     }
@@ -35,5 +73,30 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun getWeekdays(): List<String> {
+        val today = LocalDate.now()
+        val monday = today.with(DayOfWeek.MONDAY)
+        val formatter = DateTimeFormatter.ofPattern("MM월 dd일")
+
+        return (0..4).map { monday.plusDays(it.toLong()).format(formatter) }
+    }
+
+    private fun resetButtonColors() {
+        binding.apply {
+            mondayButton.strokeColor = Color.TRANSPARENT
+            tuesdayButton.strokeColor = Color.TRANSPARENT
+            wednesdayButton.strokeColor = Color.TRANSPARENT
+            thursdayButton.strokeColor = Color.TRANSPARENT
+            fridayButton.strokeColor = Color.TRANSPARENT
+        }
+    }
+
+    private fun highlightButton(button: View) {
+        button as MaterialCardView
+        val primaryColor = ContextCompat.getColor(requireContext(), R.color.primary_color)
+        button.strokeColor = primaryColor
+    }
+
 
 }
